a6143a8a2f75ee1b306ecf1ee652efaea2d5513d
you <you@example.com>
2020-09-06 11:18:28 +0000

    Added local files

--- test/UniswapV2Pair.spec.ts
+++ test/UniswapV2Pair.spec.ts
@@ -85,8 +85,8 @@ describe('UniswapV2Pair', () => {
     [2000, 1000, 3],
   ]
   const swapTestCases: BigNumber[][] = [
-    // slope, exponent, fee, swapAmount, token0AMount, token1Amount, expectedOutputAmount
-    [1, 5, 10, '1662497915624478906'], //swapAmount, token0AMount, token1Amount, expectedOutputAmount
+    // swapAmount (token 0 in), token0AMount (liquidity token 0), (liquidity toke1 ), expectedOutputAmount (token 1)
+    [1, 5, 10, '1662497915624478906'], //, token0AMount, token1Amount, expectedOutputAmount
     [1, 10, 5, '453305446940074565'],
 
     [2, 5, 10, '2851015155847869602'],
@@ -96,10 +96,9 @@ describe('UniswapV2Pair', () => {
     [1, 100, 100, '987158034397061298'],
     [1, 1000, 1000, '996006981039903216'],
 
-    // linear tests
-    // slope = 1/2
-    [1, 5, 10, '1662497915624478906'],
-    // slope = 2
+    // parameterized linear tests
+
+    [1, 50, 25, '1662497915624478906'],
     [1, 5, 10, '1662497915624478906']
 
   ].map(a => a.map(n => (typeof n === 'string' ? bigNumberify(n) : expandTo18Decimals(n))))
@@ -120,10 +119,10 @@ describe('UniswapV2Pair', () => {
 
 
   const optimisticTestCases: BigNumber[][] = [
-    ['997000000000000000', 5, 10, 1], // given amountIn, amountOut = floor(amountIn * .997)
+    ['997000000000000000', 5, 10, 1], // given amountIn, amountOut = floor(amountIn * .997 * slope)
     ['997000000000000000', 10, 5, 1],
     ['997000000000000000', 5, 5, 1],
-    [1, 5, 5, '1003009027081243732'] // given amountOut, amountIn = ceiling(amountOut / .997)
+    [1, 5, 5, '1003009027081243732'] // given amountOut, amountIn = ceiling(amountOut / (.997 * slope))
   ].map(a => a.map(n => (typeof n === 'string' ? bigNumberify(n) : expandTo18Decimals(n))))
   optimisticTestCases.forEach((optimisticTestCase, i) => {
     it(`optimistic:${i}`, async () => {
