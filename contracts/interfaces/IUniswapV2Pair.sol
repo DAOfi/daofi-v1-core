5315bd3d9d0356fcf4202d33af542928c96ea69f
you <you@example.com>
2020-09-08 07:14:28 +0000

    Restored blocktimestap to getReserves

--- contracts/interfaces/IUniswapV2Pair.sol
+++ contracts/interfaces/IUniswapV2Pair.sol
@@ -37,7 +37,7 @@ interface IUniswapV2Pair {
     function factory() external view returns (address);
     function token0() external view returns (address);
     function token1() external view returns (address);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
     function getCurveParams() external view returns (address baseToken, uint m, uint n, uint fee);
 
     function price0CumulativeLast() external view returns (uint);
--- test/UniswapV2Factory.spec.ts
+++ test/UniswapV2Factory.spec.ts
@@ -68,7 +68,7 @@ describe('UniswapV2Factory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES, TEST_ADDRESSES[0], wallet.address, 1000, 1000, 3)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2839738)
+    expect(receipt.gasUsed).to.eq(2850360)
   })
 
   it('setFeeTo', async () => {
