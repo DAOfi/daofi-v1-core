7a54ed3896ebfecbbf970473950b4b691d94723b
you <you@example.com>
2020-11-06 16:19:02 -0800

    Fix tests

--- test/UniswapV2Factory.spec.ts
+++ test/UniswapV2Factory.spec.ts
@@ -70,7 +70,7 @@ describe('UniswapV2Factory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES, TEST_ADDRESSES[0], wallet.address, expandTo18Decimals(1), 1, 3)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(3146440)
+    expect(receipt.gasUsed).to.eq(3126776)
   })
 
   it('setFeeTo', async () => {
