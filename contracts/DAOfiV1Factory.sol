884d09e93551ee926ffc518c9f65fc89c2409b05
you <you@example.com>
2020-11-14 18:01:22 -0800

    Update create2 addresses to include m, n, fee

--- contracts/DAOfiV1Factory.sol
+++ contracts/DAOfiV1Factory.sol
@@ -5,7 +5,7 @@ import './interfaces/IDAOfiV1Factory.sol';
 import './DAOfiV1Pair.sol';
 
 contract DAOfiV1Factory is IDAOfiV1Factory {
-    mapping(address => mapping(address => address)) public override getPair;
+    mapping(address => mapping(address => mapping(bytes => address))) public pairs;
     address[] public override allPairs;
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
@@ -13,23 +13,34 @@ contract DAOfiV1Factory is IDAOfiV1Factory {
     constructor() public {
     }
 
+    function getPair(address token0, address token1, uint32 m, uint32 n, uint32 fee)
+        public override view returns (address pair)
+    {
+        return pairs[token0][token1][abi.encode(m, n, fee)];
+    }
+
     function allPairsLength() external override view returns (uint) {
         return allPairs.length;
     }
 
-    function createPair(address tokenA, address tokenB, address baseToken, address pairOwner, uint32 m, uint32 n, uint32 fee) external override returns (address pair) {
+    function createPair(
+        address tokenA,
+        address tokenB,
+        address baseToken,
+        address pairOwner, uint32 m, uint32 n, uint32 fee
+    ) external override returns (address pair) {
         require(tokenA != tokenB, 'DAOfiV1: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'DAOfiV1: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'DAOfiV1: PAIR_EXISTS'); // single check is sufficient
+        require(getPair(token0, token1, m, n, fee) == address(0), 'DAOfiV1: PAIR_EXISTS'); // single check is sufficient
         bytes memory bytecode = type(DAOfiV1Pair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1, m, n, fee));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
         IDAOfiV1Pair(pair).initialize(token0, token1, baseToken, pairOwner, m, n, fee);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        pairs[token0][token1][abi.encode(m, n, fee)] = pair;
+        pairs[token1][token0][abi.encode(m, n, fee)] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
         emit PairCreated(token0, token1, pair, allPairs.length);
     }
--- contracts/interfaces/IDAOfiV1Factory.sol
+++ contracts/interfaces/IDAOfiV1Factory.sol
@@ -2,10 +2,26 @@ pragma solidity =0.6.6;
 pragma experimental ABIEncoderV2;
 
 interface IDAOfiV1Factory {
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(
+        address indexed token0,
+        address indexed token1,
+        uint32 m,
+        uint32 n,
+        uint32 fee,
+        address pair
+    );
 
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function getPair(address tokenA, address tokenB, uint32 m, uint32 n, uint32 fee)
+        external view returns (address pair);
     function allPairs(uint) external view returns (address pair);
     function allPairsLength() external view returns (uint);
-    function createPair(address tokenA, address tokenB, address baseToken, address pairOwner, uint32 m, uint32 n, uint32 fee) external returns (address pair);
+    function createPair(
+        address tokenA,
+        address tokenB,
+        address baseToken,
+        address pairOwner,
+        uint32 m,
+        uint32 n,
+        uint32 fee
+    ) external returns (address pair);
 }
