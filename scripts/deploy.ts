c62a3a3e81fecf1caf5345e873787e91b4997820
you <you@example.com>
2020-11-24 20:28:14 -0800

    Update deploy script for sokol

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@ import { deployContract } from 'ethereum-waffle'
 import DAOfiV1Factory from '../build/contracts/DAOfiV1Factory.sol/DAOfiV1Factory.json'
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider('https://dai.poa.network', 100)
+  const provider = new ethers.providers.JsonRpcProvider('https://sokol.poa.network', 0x4d)
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider)
   console.log('wallet', wallet.address)
   const factory = await deployContract(
@@ -11,12 +11,12 @@ async function main() {
     DAOfiV1Factory,
     [],
     {
-      chainId: 100,
+      chainId: 0x4d,
       gasLimit: 9999999,
       gasPrice: ethers.utils.parseUnits('120', 'gwei')
     }
   )
-  console.log('deployed factory', await factory.addressPromise)
+  console.log('deployed factory', factory.address)
 }
 
 main()
