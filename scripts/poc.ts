0a15f4eceb5b0e306951692f5bfd3ddef1c03b30
you <you@example.com>
2020-11-25 16:08:23 -0800

    Update poc.ts

--- scripts/poc.ts
+++ scripts/poc.ts
@@ -1,177 +1,30 @@
-// reserve balances for uniswap style and bonding curve
-let baseReserve = 0
-let quoteReserve = 0
-let slope = 1
-let n = 1
-let s = 0
+async function main() {
+  const MAX_DECIMALS: number =  (10 ** 5)
+  const maxTiers: number = 100000
+  // adjust these params for your curve
+  const startingPrice: number = 10.00
+  const slopeN: number = 1 // 1 - 1e6
+  const slopeD: number = 1e6 // don't change
+  const n: number = 2 // 1 - 10
 
-function logRes(useS: boolean = false) {
-  console.log(`base reserve: ${baseReserve}, quote reserve: ${quoteReserve}` + (useS ? `, s: ${s}` : ''))
-}
+  // quote price y = m(base sold x)^n
+  // given starting price y, solve for x
+  // base sold x = (quote price y/m)^(1/n)
+  const s0: number = Math.floor(((startingPrice / (slopeN / slopeD)) ** (1/n)) * MAX_DECIMALS) / MAX_DECIMALS
 
-function initRes(
-  slopeN: number,
-  slopeD: number,
-  exp: number,
-  baseR: number,
-  quoteR: number,
-) {
-  baseReserve = baseR
-  quoteReserve = quoteR
-  slope = slopeN / slopeD
-  n = exp
-  // y' = (slopeN * x ** (n + 1)) / (slopeD * (n + 1))
-  // given y' = quoteReserve and x = s, solve for s
-  s = (quoteReserve / (slope / (n + 1))) ** (1 / (n + 1))
-}
-
-// amountOut = (amountIn x reserveOut) / (amountIn + reserveIn)
-function uniswap(amountIn: number, isQuote: boolean) {
-  let amountOut = isQuote ? (amountIn * baseReserve) / (quoteReserve + amountIn) :
-    (amountIn * quoteReserve) / (baseReserve + amountIn)
-
-  if (isQuote) {
-    quoteReserve += amountIn
-    baseReserve -= amountOut
-    console.log(`swapped ${amountIn} quote for ${amountOut} base`)
-  } else {
-    quoteReserve -= amountOut
-    baseReserve += amountIn
-    console.log(`swapped ${amountIn} base for ${amountOut} quote`)
+  // quote price y = m(base sold x)x^n
+  // quote reserve y' = (m/(n+1)) * ((base sold x)^(n+1))
+  for (let x: number = s0; x < maxTiers; ++x) {
+    const price: number = Math.floor(((slopeN / slopeD) * (x ** n)) * MAX_DECIMALS) / MAX_DECIMALS
+    const reserve: number = Math.floor((((slopeN / slopeD) / (n + 1)) * (x ** (n + 1))) * MAX_DECIMALS) / MAX_DECIMALS
+    const s: number = Math.floor(x * MAX_DECIMALS) / MAX_DECIMALS
+    console.log(`supply = ${s}, price = ${price}, reserve = ${reserve}`)
   }
-
-  logRes()
-  return amountOut
 }
 
-
-// y = mx ** n
-// given y = price and x = s, solve for s
-// then plug s into the antiderivative
-// y' = (slopeN * x ** (n + 1)) / (slopeD * (n + 1))
-// y' = quote reserve at price
-function getReserveForStartPrice(price: number, slopeN: number, slopeD: number, exp: number) {
-  let s = (price * (slopeD / slopeN)) ** (1 / exp)
-  return (slope * (s ** (n + 1))) / (n + 1)
-}
-
-// y = mx ** n
-// y' = (slopeN * x ** (n + 1)) / (slopeD * (n + 1))
-// given y' = (quote in + quote reserve) and x = s, solve for s
-// subtract from previous s to get net base
-function exchangeQuote(quoteIn: number) {
-  let baseOut = (((quoteReserve + quoteIn) / (slope / (n + 1))) ** (1 / (n + 1))) - s
-  s += baseOut
-  quoteReserve += quoteIn
-  baseReserve -= baseOut
-  console.log(`swapped ${quoteIn} quote for ${baseOut} base`)
-  logRes(true)
-  return baseOut
-}
-
-// y = mx ** n
-// y' = (slopeN * x ** (n + 1)) / (slopeD * (n + 1))
-// given y'(s) = quoteReserve
-// subtract y'(s) - y'(s - k)
-function exchangeBase(baseIn: number) {
-  let quoteOut = quoteReserve - ((slope / (n + 1)) * ((s - baseIn) ** (n + 1)))
-  s -= baseIn
-  baseReserve += baseIn
-  quoteReserve -= quoteOut
-  console.log(`swapped ${baseIn} base for ${quoteOut} quote`)
-  logRes(true)
-  return quoteOut
-}
-
-console.log('\nuniswap slope 1 n 1:')
-initRes(1, 1, 1, 1000, 1000)
-uniswap(uniswap(100, true), false)
-
-console.log('\nparameterized reserve slope 1 n 1:')
-initRes(1, 1, 1, 1000, 0)
-exchangeBase(exchangeQuote(100))
-
-console.log('\nparameterized reserve slope 1 exp 2')
-initRes(1, 1, 2, 1000, 0)
-exchangeBase(exchangeQuote(100))
-
-console.log('\nparameterized reserve slope 1/2 exp 1')
-initRes(1, 2, 1, 1000, 0)
-exchangeBase(exchangeQuote(100))
-
-console.log('\nparameterized reserve slope 1/2 exp 2')
-initRes(1, 2, 2, 1000, 0)
-exchangeBase(exchangeQuote(100))
-
-console.log('\nparameterized reserve slope 2 exp 1')
-initRes(2, 1, 1, 1000, 0)
-exchangeBase(exchangeQuote(100))
-
-console.log('\nparameterized reserve slope 2 exp 2')
-initRes(2, 1, 2, 1000, 0)
-exchangeBase(exchangeQuote(100))
-
-// continuous token model
-let tokensMinted = 0
-let poolBalance = 0
-
-function logCont() {
-  console.log(`tokens minted: ${tokensMinted}, pool balance: ${poolBalance}`)
-}
-
-function initCont(
-  slopeN: number,
-  slopeD: number,
-  exp: number
-) {
-  tokensMinted = 0
-  poolBalance = 0
-  slope = slopeN / slopeD
-  n = exp
-}
-
-function buyTokens(poolIn: number) {
-  let tokensOut = (((poolBalance + poolIn) / (slope / (n + 1))) ** (1 / (n + 1))) - tokensMinted
-  tokensMinted += tokensOut
-  poolBalance += poolIn
-  console.log(`swapped ${poolIn} pool in for ${tokensOut} tokens`)
-  logCont()
-  return tokensOut
-}
-
-function sellTokens(tokensIn: number) {
-  let poolOut = poolBalance - ((slope / (n + 1)) * ((tokensMinted - tokensIn) ** (n + 1)))
-  tokensMinted -= tokensIn
-  poolBalance -= poolOut
-  console.log(`swapped ${tokensIn} tokens for ${poolOut} pool out`)
-  logCont()
-  return poolOut
-}
-
-console.log('\ncontinous slope 1 n 1:')
-initCont(1, 1, 1)
-sellTokens(buyTokens(100))
-
-console.log('\ncontinuous slope 1 exp 2')
-initCont(1, 1, 2)
-sellTokens(buyTokens(100))
-
-console.log('\ncontinuous slope 1/2 exp 1')
-initCont(1, 2, 1)
-sellTokens(buyTokens(100))
-
-console.log('\ncontinuous slope 1/2 exp 2')
-initCont(1, 2, 2)
-sellTokens(buyTokens(100))
-
-console.log('\ncontinuous slope 2 exp 1')
-initCont(2, 1, 1)
-sellTokens(buyTokens(100))
-
-console.log('\ncontinuous slope 2 exp 2')
-initCont(2, 1, 2)
-sellTokens(buyTokens(100))
-
-
-
-
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  });
