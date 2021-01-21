69e5b8d76885b634638a449c2112122839a6f397
you <you@example.com>
2021-01-21 12:13:00 -0800

    Fix deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,6 @@ import BancorFormula from '@daofi/bancor/solidity/build/contracts/BancorFormula.
  import { ethers } from 'hardhat'
 import { deployContract } from 'ethereum-waffle'
 import DAOfiV1Factory from '../build/contracts/DAOfiV1Factory.sol/DAOfiV1Factory.json'
-import BancorFormula from '@daofi/bancor/solidity/build/contracts/BancorFormula.json'
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
@@ -10,23 +9,6 @@ async function main() {
   )
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider)
   console.log('wallet', wallet.address)
-<<<<<<< HEAD
-  const formula = await deployContract(
-    wallet,
-    BancorFormula as any, [], {
-      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 0x2a,
-      gasLimit: 9999999,
-      gasPrice: ethers.utils.parseUnits('120', 'gwei')
-    }
-  )
-  console.log('deployed formula', formula.address)
-  const factory = await deployContract(
-    wallet,
-    DAOfiV1Factory,
-    [formula.adress],
-    {
-      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 0x2a,
-=======
 
   const formula = await deployContract(wallet, BancorFormula as any) //waffle doesn't like the type from truffle
   console.log('New BancorFormula deployed at: ' + formula.address)
@@ -43,7 +25,6 @@ async function main() {
     {
       nonce: nonce,
       chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 0x2a, // kovan default
->>>>>>> a6247b92b6073ade9d3576b12d348016bd4c7a7a
       gasLimit: 9999999,
       gasPrice: ethers.utils.parseUnits('120', 'gwei')
     }
