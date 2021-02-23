1b2224af6fdfc93038be798d6289cc143cce56aa
you <you@example.com>
2021-02-23 10:29:45 -0800

    Update deploy script for sokol

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,20 @@
 import BancorFormula from '@daofi/bancor/solidity/build/contracts/BancorFormula.json'
- import { ethers } from 'hardhat'
+ import { ethers } from 'ethers'
 import { deployContract } from 'ethereum-waffle'
 import DAOfiV1Factory from '../build/contracts/DAOfiV1Factory.sol/DAOfiV1Factory.json'
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    process.env.JSONRPC_URL || 'https://kovan.poa.network'
+    process.env.JSONRPC_URL || 'https://sokol.poa.network'
   )
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider)
-  console.log('wallet', wallet.address)
+  console.log('Wallet:', wallet.address)
 
   const formula = await deployContract(wallet, BancorFormula as any) //waffle doesn't like the type from truffle
-  console.log('New BancorFormula deployed at: ' + formula.address)
+  console.log('New BancorFormula deployed at:', formula.address)
 
   await formula.init()
-  console.log('Formula initialized')
+  console.log('Formula initialized.')
 
   const nonce = await wallet.getTransactionCount()
 
@@ -24,12 +24,12 @@ async function main() {
     [formula.address],
     {
       nonce: nonce,
-      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 0x2a, // kovan default
+      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 0x4D, // default to sokol (77)
       gasLimit: 9999999,
       gasPrice: ethers.utils.parseUnits('120', 'gwei')
     }
   )
-  console.log('deployed factory', factory.address)
+  console.log('Factory deployed at:', factory.address)
 }
 
 main()
